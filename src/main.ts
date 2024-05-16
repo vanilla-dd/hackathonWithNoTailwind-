3744eac06ed558009c8f964ea3a9a21cba490f51
vanilla-dd <freeidlll6@gmail.com>
2024-05-16 17:18:01 +0530

    feat: cursor

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import "./style.css";
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.querySelector(".cursor");
+  let mouseX = 0;
+  let mouseY = 0;
+  let cursorX = 0;
+  let cursorY = 0;
+
+  document.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function delayCursor() {
+    const distX = mouseX - cursorX;
+    const distY = mouseY - cursorY;
+    cursorX = cursorX + distX * 0.1; // Adjust the speed of the cursor
+    cursorY = cursorY + distY * 0.1; // Adjust the speed of the cursor
+    cursor.style.left = cursorX + "px";
+    cursor.style.top = cursorY + "px";
+    requestAnimationFrame(delayCursor);
+  }
+
+  delayCursor();
+});
